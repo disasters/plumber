c908c7f4cb35a7c1bbb300cbc3350ff7bc4640bd
Tyler Neely <t@jujit.su>
2015-07-26 21:23:19 -0700

    POC for basic replacement of getaddrinfo and connect arguments.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use libc::types::os::common::bsd44::{addrinfo, socklen_t, sockaddr};
 use libc::{c_char, c_int, c_void};
 use std::env;
 use std::mem;
+use std::ffi::{CString, CStr};
+use std::str::from_utf8;
 use std::sync::{Once, ONCE_INIT, Arc, Mutex};
 
 use discotech::*;
@@ -27,7 +29,8 @@ struct SingletonServerset {
     inner: Arc<Mutex<Serverset>>
 }
 
-fn singleton() -> SingletonServerset {
+// Please excuse this.
+fn get_serverset() -> SingletonServerset {
     static mut SINGLETON: *const SingletonServerset = 0 as *const SingletonServerset;
     static ONCE: Once = ONCE_INIT;
 
@@ -37,47 +40,45 @@ fn singleton() -> SingletonServerset {
             let conf = read_config(conf_path).unwrap();
             let s = Serverset::new(conf);
 
-            let singleton = SingletonServerset {
+            let serverset = SingletonServerset {
                 inner: Arc::new(Mutex::new(s))
             };
 
-            // Put it in the heap so it can outlive this call
-            SINGLETON = mem::transmute(Box::new(singleton));
-
-            // Make sure to free heap memory at exit
-            /* This doesn't exist in stable 1.0, so we will just leak it!
-            rt::at_exit(|| {
-                let singleton: Box<SingletonReader> = mem::transmute(SINGLETON);
-
-                // Let's explictly free the memory for this example
-                drop(singleton);
-
-                // Set it to null again. I hope only one thread can call `at_exit`!
-                SINGLETON = 0 as *const _;
-            });
-            */
+            SINGLETON = mem::transmute(Box::new(serverset));
         });
 
-        // Now we give out a copy of the data that is safe to use concurrently.
         (*SINGLETON).clone()
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn connect(socket: c_int, address: *const sockaddr,
+pub unsafe extern "C" fn connect(socket: c_int, address: *mut sockaddr,
                            len: socklen_t) -> c_int {
-    println!("HOOKING CONNECT!!!!!!!!!!!!!!!!!");
-    let ss = singleton();
+    println!("connect pre-hook: {:?}", (*address).sa_data);
+    if (*address).sa_data[1] == 79 {
+        (*address).sa_data[1] = 80;
+    }
+    println!("connect post-hook: {:?}", (*address).sa_data);
+    //let ss = get_serverset();
     let ptr = dlsym_next("connect");
     let f: fn(c_int, *const sockaddr, socklen_t) -> c_int = mem::transmute(ptr);
     f(socket, address, len)
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn getaddrinfo(node: *const c_char, service: *const c_char,
+pub unsafe extern "C" fn getaddrinfo(mut node: *const c_char, service: *const c_char,
                hints: *const addrinfo, res: *const *const addrinfo) -> c_int {
-    println!("HOOKING getaddrinfo!!!!!!!!!!!!!!!!!");
-    let ss = singleton();
+    let c_str = unsafe { CStr::from_ptr(node) };
+    let s = from_utf8(c_str.to_bytes()).unwrap().to_owned();
+    println!("getaddrinfo pre-hook: node: {:?} service: {:?}", s, service);
+
+    let new_host = CString::new("google.com".as_bytes()).unwrap();
+    node = new_host.as_ptr();
+    //let ss = get_serverset();
+
+    let c_str = unsafe { CStr::from_ptr(node) };
+    let s = from_utf8(c_str.to_bytes()).unwrap().to_owned();
+    println!("getaddrinfo pe3-hook: node: {:?} service: {:?}", s, service);
     let ptr = dlsym_next("getaddrinfo");
     let f: fn(*const c_char, *const c_char, *const addrinfo,
               *const *const addrinfo) -> c_int = mem::transmute(ptr);
