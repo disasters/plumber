d35c99c353a0788ae99ae150651e9f72733f2b1d
Tyler Neely <t@jujit.su>
2015-07-26 20:00:26 -0700

    Add some horrible singleton hacks, change name to plumber.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-docs [here](github.com/the-tetanus-clinic/discotech)
+# Plumber
+Plumber is the code-name for Project B-Hole.  ~~Project B-Hole~~Plumber is an LD_PRELOAD hooking library for [Discotech](https://github.com/the-tetanus-clinic/discotech), providing lazy initialization and hooks of connect and getaddrinfo.
+
+## Usage
+Linux/FreeBSD:
+```
+DISCO_CONF=/path/to/discotech.conf.json \
+LD_PRELOAD=target/debug/libplumber.so \
+$PROGRAM
+```
+
+OSX:
+```
+DISCO_CONF=/path/to/discotech.conf.json \
+DYLD_INSERT_LIBRARIES=/abs/path/to/libplumber.so \
+DYLD_FORCE_FLAT_NAMESPACE=YES \
+$PROGRAM
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 #![feature(libc)]
 extern crate libc;
+extern crate discotech;
 use libc::types::os::common::bsd44::{addrinfo, socklen_t, sockaddr};
 use libc::{c_char, c_int, c_void};
+use std::env;
 use std::mem;
+use std::sync::{Once, ONCE_INIT, Arc, Mutex};
+
+use discotech::*;
 
 #[link(name="dl")]
 extern {
@@ -17,10 +22,52 @@ pub unsafe fn dlsym_next(symbol: &'static str) -> *const u8 {
     ptr as *const u8
 }
 
+#[derive(Clone)]
+struct SingletonServerset {
+    inner: Arc<Mutex<Serverset>>
+}
+
+fn singleton() -> SingletonServerset {
+    static mut SINGLETON: *const SingletonServerset = 0 as *const SingletonServerset;
+    static ONCE: Once = ONCE_INIT;
+
+    unsafe {
+        ONCE.call_once(|| {
+            let conf_path = env::var("DISCO_CONF").unwrap();
+            let conf = read_config(conf_path).unwrap();
+            let s = Serverset::new(conf);
+
+            let singleton = SingletonServerset {
+                inner: Arc::new(Mutex::new(s))
+            };
+
+            // Put it in the heap so it can outlive this call
+            SINGLETON = mem::transmute(Box::new(singleton));
+
+            // Make sure to free heap memory at exit
+            /* This doesn't exist in stable 1.0, so we will just leak it!
+            rt::at_exit(|| {
+                let singleton: Box<SingletonReader> = mem::transmute(SINGLETON);
+
+                // Let's explictly free the memory for this example
+                drop(singleton);
+
+                // Set it to null again. I hope only one thread can call `at_exit`!
+                SINGLETON = 0 as *const _;
+            });
+            */
+        });
+
+        // Now we give out a copy of the data that is safe to use concurrently.
+        (*SINGLETON).clone()
+    }
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn connect(socket: c_int, address: *const sockaddr,
                            len: socklen_t) -> c_int {
     println!("HOOKING CONNECT!!!!!!!!!!!!!!!!!");
+    let ss = singleton();
     let ptr = dlsym_next("connect");
     let f: fn(c_int, *const sockaddr, socklen_t) -> c_int = mem::transmute(ptr);
     f(socket, address, len)
@@ -30,6 +77,7 @@ pub unsafe extern "C" fn connect(socket: c_int, address: *const sockaddr,
 pub unsafe extern "C" fn getaddrinfo(node: *const c_char, service: *const c_char,
                hints: *const addrinfo, res: *const *const addrinfo) -> c_int {
     println!("HOOKING getaddrinfo!!!!!!!!!!!!!!!!!");
+    let ss = singleton();
     let ptr = dlsym_next("getaddrinfo");
     let f: fn(*const c_char, *const c_char, *const addrinfo,
               *const *const addrinfo) -> c_int = mem::transmute(ptr);
